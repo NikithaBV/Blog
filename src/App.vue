e755541385637923db8e6956ac78c56d2b89f23a
Nikitha <nikithabv5@gmail.com>
2025-03-26 14:52:02 +0530

    all files added

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <app-header></app-header>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// Imports
+
+import headerAdd from './components/headerAdd.vue';
+
+export default {
+  components: {
+      'app-header': headerAdd
+  },
+  data () {
+      return {
+
+      }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
---
+++ src/components/singleBlog.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="single-blog">
+        <h1>{{ blog.title }}</h1>
+        <article>{{ blog.body }}</article>
+
+    </div>
+</template>
+
+<script>
+// id:this.$route.params.id,(fetches id from url using vue router)
+//this.$http.get('http://jsonplaceholder.typicode.com/posts/'+ this.id)  
+//makes http request and get blog id
+export default{
+    data(){
+        return{
+            id:this.$route.params.id,
+            blog:{}
+        }
+    },
+
+    created()
+    {
+        this.$http.get('http://jsonplaceholder.typicode.com/posts/'+ this.id).then(function(data)
+    {
+        console.log(data);
+        this.blog=data.body;
+    })
+    }
+}
+</script>
+
+<style scoped>
+#single-blog{
+    max-width: 940px;
+    margin: 0 auto;
+}
+</style>
